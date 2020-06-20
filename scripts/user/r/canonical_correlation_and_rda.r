328b9b7201ab25a715d2dc120a51bef39eb14d86
JBris <james.a.bristow.contact@gmail.com>
2020-06-21 01:02:43 +1200

    Scaling CCA and RDA

--- scripts/user/r/canonical_correlation_and_rda.r
+++ scripts/user/r/canonical_correlation_and_rda.r
@@ -7,10 +7,10 @@ data <- read_csv("data/concrete.csv")
 X <- data %>% select(c("cement", "slag", "fly_ash", "water", "superplasticizer", "coarse_aggregate", "fine_aggregate"))
 Y <- data %>% select(c("slump", "flow", "compressive_strength"))
 
-concrete_cca <- data %>% cca(X, Y)
+concrete_cca <- data %>% cca(X, Y, scale = T)
 concrete_cca %>% structure()
 plot(concrete_cca)
 
-concrete_rda <- data %>% rda(X, Y)
+concrete_rda <- data %>% rda(X, Y, scale = T)
 concrete_rda %>% structure()
 plot(concrete_rda)
