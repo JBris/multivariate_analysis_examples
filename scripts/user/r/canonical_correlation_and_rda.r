3e768649e9060fe2bead6fdcbc985ca28bd7983a
JBris <james.a.bristow.contact@gmail.com>
2020-07-27 21:07:41 +1200

    Applying fix for CCA in R

--- scripts/user/r/canonical_correlation_and_rda.r
+++ scripts/user/r/canonical_correlation_and_rda.r
@@ -2,14 +2,14 @@ library(readr)
 library(tidyverse)
 library(dplyr)
 library(vegan)
+library(CCA)
 
 data <- read_csv("data/concrete.csv")  
 X <- data %>% select(c("cement", "slag", "fly_ash", "water", "superplasticizer", "coarse_aggregate", "fine_aggregate"))
 Y <- data %>% select(c("slump", "flow", "compressive_strength"))
 
-concrete_cca <- data %>% cca(X, Y, scale = T)
-concrete_cca %>% structure()
-plot(concrete_cca)
+concrete_cca <- cc(X, Y)
+comput(X, Y, concrete_cca) %>% structure()
 
 concrete_rda <- data %>% rda(X, Y, scale = T)
 concrete_rda %>% structure()
